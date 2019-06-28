10417778991f61c1ba0e4b693ef82eb84c3392c3
Aldo Pietrangeli <pietrangeli.aldo@gmail.com>
2019-06-28 15:26:17 +0200

    Query 1 con kafka e windowall

--- src/main/java/myflink/SocketWindowWordCount.java
+++ src/main/java/myflink/SocketWindowWordCount.java
@@ -1,17 +1,15 @@
 package myflink;
 import myflink.utils.CommentLogSchema;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
+
 import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
-import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
@@ -52,65 +50,49 @@ public class SocketWindowWordCount {
                 });
 
         // calcola quanto un articolo e' popolare con finestra sliding
-        DataStream<Tuple3<String, CommentLog, Integer>> my_sum = timestampedAndWatermarked
+        DataStream<Tuple3<String, CommentLog, Integer>> classifica = timestampedAndWatermarked
                 .keyBy(value -> value.f0.articleID)
-                .timeWindow(Time.minutes(60))
+                .timeWindow(Time.minutes(120))
                 .sum(1)
-                .map(new MapFunction<Tuple2<CommentLog, Integer>, Tuple3<String, CommentLog, Integer>>() {
+                .timeWindowAll(Time.minutes(120))
+                .process(new ProcessAllWindowFunction<Tuple2<CommentLog, Integer>, Tuple3<String, CommentLog, Integer>, TimeWindow>() {
                     @Override
-                    public Tuple3<String, CommentLog, Integer> map(Tuple2<CommentLog, Integer> logIntegerTuple2) throws Exception {
-
-                        return new Tuple3<>("label", logIntegerTuple2.f0, logIntegerTuple2.f1);
-                    }
-                });
-
-
-        // stampa la classifica con finestra tumbling
-        DataStream<Tuple2<CommentLog, Integer>> resultWind = my_sum
-                .keyBy(0)
-                .timeWindow(Time.minutes(60))
-                .apply(new WindowFunction<Tuple3<String, CommentLog, Integer>, Tuple2<CommentLog, Integer>, Tuple, TimeWindow>() {
-                    @Override
-                    public void apply(Tuple tuple, TimeWindow timeWindow,
-                                      Iterable<Tuple3<String, CommentLog, Integer>> iterable,
-                                      Collector<Tuple2<CommentLog, Integer>> collector) throws Exception {
-
+                    public void process(Context context, Iterable<Tuple2<CommentLog, Integer>> iterable, Collector<Tuple3<String, CommentLog, Integer>> collector) throws Exception {
                         List<Tuple2<Integer, CommentLog>> tuple2s = new ArrayList<Tuple2<Integer, CommentLog>>();
 
-                        for(Tuple3<String, CommentLog, Integer> my_tuple : iterable){
-                            tuple2s.add( new Tuple2<Integer, CommentLog> (my_tuple.f2, my_tuple.f1));
+                        for (Tuple2<CommentLog, Integer> my_tuple : iterable) {
+                            tuple2s.add(new Tuple2<Integer, CommentLog>(my_tuple.f1, my_tuple.f0));
                         }
                         Collections.sort(tuple2s, new Comparator<Tuple2<Integer, CommentLog>>() {
                             @Override
                             public int compare(Tuple2<Integer, CommentLog> o1, Tuple2<Integer, CommentLog> o2) {
-                                int v1 =  o1.f0.intValue();
-                                int v2 =  o2.f0.intValue();
-                                return v2-v1;
+                                int v1 = o1.f0.intValue();
+                                int v2 = o2.f0.intValue();
+                                return v2 - v1;
                             }
                         });
 
                         System.out.println("--------------------");
-                        Date date_start = new Date(timeWindow.getStart());
-                        Date date_end = new Date(timeWindow.getEnd());
-                        System.out.println("size: "+tuple2s.size());
-                        System.out.println("Time Window Start "+date_start.toString());
-                        System.out.println("Time Window End: "+date_end.toString());
-                        if(tuple2s.size() > 0){
-                            System.out.println("\n First: "+tuple2s.get(0));
+                        Date date_start = new Date(context.window().getStart());
+                        Date date_end = new Date(context.window().getEnd());
+                        System.out.println("size: " + tuple2s.size());
+                        System.out.println("Time Window Start " + date_start.toString());
+                        System.out.println("Time Window End: " + date_end.toString());
+                        if (tuple2s.size() > 0) {
+                            System.out.println("\n First: " + tuple2s.get(0));
                         }
-                        if(tuple2s.size() > 1){
-                            System.out.println("\n Second: "+tuple2s.get(1));
+                        if (tuple2s.size() > 1) {
+                            System.out.println("\n Second: " + tuple2s.get(1));
                         }
-                        if(tuple2s.size() > 2){
-                            System.out.println("\n Third: "+tuple2s.get(2));
+                        if (tuple2s.size() > 2) {
+                            System.out.println("\n Third: " + tuple2s.get(2));
                         }
 
                         System.out.println("-------------------");
                     }
                 });
 
-        // Print the results to the console, note that here it uses the single-threaded printing instead of multi-threading
-        resultWind.print().setParallelism(1);
+        classifica.print().setParallelism(1);
 
         env.execute("Socket Window WordCount");
 
