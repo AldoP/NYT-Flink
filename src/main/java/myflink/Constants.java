454c7b89c6c3fc3da1e22e299e9d4d4542366798
Aldo Pietrangeli <pietrangeli.aldo@gmail.com>
2019-07-11 20:35:18 +0200

    add metric

--- src/main/java/myflink/Constants.java
+++ src/main/java/myflink/Constants.java
@@ -9,5 +9,9 @@ public final class Constants {
     public static final String QUERY3_PATHOUT = "query3";
     public static final String QUERY2_PATHOUT = "query2";
     public static final String QUERY1_PATHOUT = "query1";
+    public static final String QUERY1_METRIC_PATHOUT = "metric_query1";
+    public static final String QUERY3_METRIC_PATHOUT = "metric_query3";
+
+
     public static final String BASE_PATH = "output/";
 }
